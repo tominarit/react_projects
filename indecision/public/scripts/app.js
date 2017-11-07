e13583f2412071f34c927f8ad89199ac392f9d15
tominari <tominari@hitokuse.com>
2017-11-07 23:11:44 +0900

    added react basics

---
+++ indecision/public/scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// //JSX JavaScript XML
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App"
+);
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
---
+++ indecision/src/app.js
@@ -0,0 +1,4 @@
+// //JSX JavaScript XML
+var template = <h1>Indecision App</h1>;
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
