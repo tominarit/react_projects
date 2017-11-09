d91ab79b13c9e0a3725d731013142512f3f8e1f0
tominari <tominari@hitokuse.com>
2017-11-09 22:41:37 +0900

    conditional rendering in JSX

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -1,10 +1,9 @@
 "use strict";
 
-// JSX JavaScript XML
-
 var app = {
   title: "Indecision",
-  subtitle: "Put your life in the hands of a computer"
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
 };
 
 var template = React.createElement(
@@ -15,10 +14,15 @@ var template = React.createElement(
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     "p",
     null,
     app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
   ),
   React.createElement(
     "ol",
--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,14 +1,14 @@
-// JSX JavaScript XML
-
 var app = {
   title: "Indecision",
-  subtitle: "Put your life in the hands of a computer"
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
 };
 
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
       <li>Item</li>
       <li>Item</li>
