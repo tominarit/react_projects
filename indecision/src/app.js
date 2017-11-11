30924922fac52b1ab5619673745fde6a20944f4e
tominari <tominari@hitokuse.com>
2017-11-12 00:30:27 +0900

    manual data binding

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -19,27 +19,32 @@ const template = (
 let count = 0;
 
 const addOne = () => {
-  console.log("AddOne");
+  count++;
+  renderCounterApp();
 };
 
 const minusOne = () => {
-  console.log("MinusOne");
+  count--;
+  renderCounterApp();
 };
 
 const reset = () => {
-  console.log("reset");
+  count = 0;
+  renderCounterApp();
 };
 
-const templateTwo = (
-  <div>
-  <h1>Count: {count}</h1>
-  <button onClick={addOne}>+1</button>
-  <button onClick={minusOne}>-1</button>
-  <button onClick={reset}>reset</button>
-  </div>
-);
-
-console.log(templateTwo);
-
 const appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
+    </div>
+  );
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
