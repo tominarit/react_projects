0e1ba94049e1819d304471ac68c07f09ebdbf431
tominari <tominari@hitokuse.com>
2017-11-17 22:03:08 +0900

    events and methods

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -26,19 +26,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert("handlePick");
+  }
   render() {
     return (
       <div>
-        <button>What I should I do?</button>
+        <button onClick={this.handlePick}>What I should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert("handleRemoveAll");
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -58,10 +65,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    
+    const option = e.target.elements.option.value.trim();
+    
+    if(option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
-        AddOption component here
+        <form onSubmit={this.handleAddOption}>
+          <input type="rext" name="option"></input>
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
