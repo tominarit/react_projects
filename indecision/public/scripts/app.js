31d01ef3bf3739ddb7c234596c6f57ad4b3aa9c5
tominari <tominari@hitokuse.com>
2017-12-28 18:06:51 +0900

    added defaultProps

--- indecision/public/scripts/app.js
+++ indecision/public/scripts/app.js
@@ -20,7 +20,7 @@ var Indecision = function (_React$Component) {
     _this.handlePick = _this.handlePick.bind(_this);
     _this.handleAddOption = _this.handleAddOption.bind(_this);
     _this.state = {
-      options: []
+      options: props.options
     };
     return _this;
   }
@@ -58,12 +58,11 @@ var Indecision = function (_React$Component) {
   }, {
     key: "render",
     value: function render() {
-      var title = "Indecision";
       var subtitle = "Put your life in the hands of a computer";
       return React.createElement(
         "div",
         null,
-        React.createElement(Header, { title: title, subtitle: subtitle }),
+        React.createElement(Header, { subtitle: subtitle }),
         React.createElement(Action, {
           handlePick: this.handlePick,
           hasOptions: this.state.options.length > 0
@@ -82,6 +81,10 @@ var Indecision = function (_React$Component) {
   return Indecision;
 }(React.Component);
 
+Indecision.defaultProps = {
+  options: []
+};
+
 var Header = function Header(props) {
   return React.createElement(
     "div",
@@ -91,12 +94,16 @@ var Header = function Header(props) {
       null,
       props.title
     ),
-    React.createElement(
+    props.subtitle && React.createElement(
       "h2",
       null,
       props.subtitle
     )
   );
+};
+
+Header.defaultProps = {
+  title: "Indecision"
 };
 
 var Action = function Action(props) {
--- indecision/src/app.js
+++ indecision/src/app.js
@@ -5,7 +5,7 @@ class Indecision extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     }
   }
   
@@ -37,11 +37,10 @@ class Indecision extends React.Component {
   }
   
   render() {
-    const title = "Indecision";
     const subtitle = "Put your life in the hands of a computer";
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           handlePick={this.handlePick}
           hasOptions={this.state.options.length > 0}
@@ -58,14 +57,22 @@ class Indecision extends React.Component {
   }
 }
 
+Indecision.defaultProps = {
+  options: []
+}
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      { props.subtitle && <h2>{props.subtitle}</h2> }
     </div>
   );
 };
+
+Header.defaultProps = {
+  title: "Indecision"
+}
 
 const Action = (props) => {
   return (
