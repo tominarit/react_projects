316e4b7be3df53941966cc6151074dfdadc9a70f
tominari <tominari@hitokuse.com>
2018-01-14 18:30:19 +0900

    setup babel with webpack

--- indecision/src/app.js
+++ indecision/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'Test');
+const template = <p>Test</p>;
 ReactDOM.render(template, document.getElementById('app'));
